8c31527181a487119a7af02c1f09a480287fd40d
joost <joost.vantschip@ka.prorail.nl>
2022-03-18 09:55:02 +0100

    Add documentation and make tests more generic

--- openspoor/mapservices/find_mapservice.py
+++ openspoor/mapservices/find_mapservice.py
@@ -4,12 +4,22 @@ import re
 
 
 class FeatureServerOverview:
+    """
+    Class used to find all the available featureservers in mapservices.
+    This can be used to navigate the API through python, allowing some more efficient searching.
+    """
     def __init__(self):
         self.prefix = 'https://mapservices.prorail.nl/'
         self.base_url = "https://mapservices.prorail.nl/arcgis/rest/services"
         self.df = self.get_all_featureserver_layers()
 
-    def _get_layers_in_featureservers(self, featureserver_url):
+    def _get_layers_in_featureservers(self, featureserver_url: str) -> pd.DataFrame:
+        """
+        For a given featureserver submenu within the mapservices environment give all the possible layers.
+
+        :param featureserver_url: The url of the selected featureserver
+        :return: A pandas dataframe, listing the urls and descriptions of the found layers
+        """
         featureserver_page = requests.get(featureserver_url)
 
         featureserver_text = featureserver_page.text
@@ -17,7 +27,12 @@ class FeatureServerOverview:
         featureservers = [[f'{self.prefix}{fs}', description] for fs, description in featureservers]
         return pd.DataFrame(featureservers, columns=['layer_url', 'description'])
 
-    def get_all_featureserver_layers(self):
+    def get_all_featureserver_layers(self) -> pd.DataFrame:
+        """
+        Find all available layers within the mapservices featureservers.
+
+        :return: A pandas dataframe, listing the urls and descriptions of the found layers in all featureservers
+        """
         all_services = requests.get(self.base_url).text
         featureserver_redirects = re.findall(r'href="/(.+/FeatureServer)"', all_services)
 
@@ -28,5 +43,11 @@ class FeatureServerOverview:
             .reset_index(drop=True)
         )
 
-    def search_for(self, search_for):
+    def search_for(self, search_for: str) -> pd.DataFrame:
+        """
+        Find all layers which include a certain phrase.
+
+        :param search_for: A case unsensitive string for which you want to find all available layers
+        :return: A pandas dataframe, listing the urls and descriptions of the found layers in all featureservers
+        """
         return self.df.loc[lambda d: d.description.str.lower().str.contains(search_for.lower())]
--- tests/test_find_mapservice.py
+++ tests/test_find_mapservice.py
@@ -7,20 +7,22 @@ def all_featureserver_layers():
     return FeatureServerOverview()
 
 
-# TODO: These tests can be a lot stricter
 def test_get_all_featureserver_layers(all_featureserver_layers):
     out = all_featureserver_layers.df
-    assert out.shape == (264, 2)
+    assert out.shape[0] > 0, 'No featureservices found'
+    assert out.shape[1] == 2, 'Unexpected number of columns'
 
 
-# TODO: These tests can be a lot stricter
 def test_get_layers_in_featureservers(all_featureserver_layers):
     url = 'https://mapservices.prorail.nl/arcgis/rest/services/Bodemkwaliteit_001/FeatureServer'
     out = all_featureserver_layers._get_layers_in_featureservers(url)
-    assert out.shape == (3, 2)
+    assert out.shape[0] > 0, 'No featureservices found'
+    assert out.shape[1] == 2, 'Unexpected number of columns'
 
 
-# TODO: These tests can be a lot stricter
 def test_search_for(all_featureserver_layers):
-    out = all_featureserver_layers.search_for('hek')
-    assert out.shape == (3, 2)
+    out_hek = all_featureserver_layers.search_for('hek')
+    out_spoor = all_featureserver_layers.search_for('spoor')
+    assert out_spoor.shape[0] > out_hek.shape[0], 'Spoor does not occur more often than hek'
+    assert out_hek.shape[1] == 2, 'Invalid number of columns'
+    assert out_spoor.shape[1] == 2, 'Invalid number of columns'
