07517d1bfaf98541634f4be70f15e83a29a6e7ac
Nozziel <marcovdlinden@gmail.com>
2022-05-09 09:34:45 +0200

    changed skip to skip on linux only.

--- tests/spoortakmodel/test_spoortak_model_mapper.py
+++ tests/spoortakmodel/test_spoortak_model_mapper.py
@@ -2,14 +2,17 @@ import pathlib
 import unittest
 from unittest import skip
 
+import pytest
+
 from openspoor.spoortakmodel import SpoortakSubsection, SpoortakModelsData
 from openspoor.spoortakmodel import SpoortakModelMapper
+import platform
 
 MODELS_DATA_DIR = str(pathlib.Path(__file__).parent.resolve().joinpath('..', '..', 'data').resolve())
 
 
 class TestSpoortakModelMapper(unittest.TestCase):
-    @skip('Fails on ubuntu but succeeds on windows')
+    @pytest.mark.skipif(platform.system() == "Linux", reason="Fails on ubuntu but succeeds on windows")
     def test_map_happy_flow(self):
         expected_models = [14, 15, 16, 17]
         subsection = SpoortakSubsection('478_1201V_0.6', 535, 570)
@@ -20,7 +23,7 @@ class TestSpoortakModelMapper(unittest.TestCase):
 
         self.assertCountEqual(expected_models, result_models)
 
-    @skip('Fails on ubuntu but succeeds on windows')
+    @pytest.mark.skipif(platform.system() == "Linux", reason="Fails on ubuntu but succeeds on windows")
     def test_map_happy_flow_bug(self):
         """ test to see if the start and end were the cause of the original test failing """
         expected_models = [14, 15, 16, 17]
@@ -32,7 +35,7 @@ class TestSpoortakModelMapper(unittest.TestCase):
 
         self.assertCountEqual(expected_models, result_models)
 
-    @skip('Fails on ubuntu but succeeds on windows')
+    @pytest.mark.skipif(platform.system() == "Linux", reason="Fails on ubuntu but succeeds on windows")
     def test_map_spoortak_id_change_assign(self):
         subsection = SpoortakSubsection('087_1321R_24.1', 24059, 25900)
 
@@ -41,7 +44,7 @@ class TestSpoortakModelMapper(unittest.TestCase):
 
         self.assertEqual(15, len(result))
 
-    @skip('Fails on ubuntu but succeeds on windows')
+    @pytest.mark.skipif(platform.system() == "Linux", reason="Fails on ubuntu but succeeds on windows")
     def test_map_to_happy_flow(self):
         subsection = SpoortakSubsection('087_1321R_24.1', 24059, 25900)
 
