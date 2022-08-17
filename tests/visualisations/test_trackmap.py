4bd98a4656a47c1c0dc1078f9251aede85d07c2f
joost <joost.vantschip@ka.prorail.nl>
2022-08-17 15:43:11 +0200

    Improve testing

--- tests/visualisations/test_trackmap.py
+++ tests/visualisations/test_trackmap.py
@@ -17,7 +17,9 @@ def emptytrackmap():
 
 @pytest.fixture(scope='session')
 def example_plottingdataframe():
-    return PlottingPoints({'lat': [52.45, 52.5], 'lon': [5.15, 5.2], 'name': ['ABC', 'DEF']})
+    return PlottingPoints({'lat': [52.45, 52.5],
+                           'lon': [5.15, 5.2],
+                           'name': ['ABC', 'DEF']})
 
 
 @pytest.fixture()
@@ -26,10 +28,11 @@ def prefilled_trackmap(example_plottingdataframe):
 
 
 def test_TrackMap(emptytrackmap, prefilled_trackmap, example_plottingdataframe):
-    objects_in_empty = len(emptytrackmap.__dict__['_children'])
-    objects_in_nonempty = len(prefilled_trackmap.__dict__['_children'])
+    objects_in_empty = len(emptytrackmap._children)
+    objects_in_nonempty = len(prefilled_trackmap._children)
     assert objects_in_empty == 1, 'Should contain only the aerial photo'
-    assert objects_in_nonempty == 1 + len(example_plottingdataframe.data), 'Should contain the aerial photo and some markers'
+    assert objects_in_nonempty == 1 + len(
+        example_plottingdataframe.data), 'Should contain the aerial photo and some markers'
 
 
 @pytest.fixture
@@ -37,7 +40,8 @@ def points_dataframe():
     return pd.DataFrame(data={'lat': [52.45, 52.5, 52.55, 52.6], 'lon': [5.15, 5.2, 5.3, 5.4],
                               'name': ['A', 'B', "C", 'D'],
                               'name2': ['AA', 'BB', "CC", 'DD'],
-                              'value': [1, 2, 3, 4]
+                              'value': [1, 2, 3, 4],
+                              "marker": ['train', "eye", 'train', "eye"]
                               })
 
 
@@ -64,16 +68,16 @@ def areas_geodataframe():
 
     return (
         pd.DataFrame(data={'name': ['M', 'N'], 'name2': ['MM', 'NN'], 'geometry': [area1, area2]})
-                .assign(geometry=lambda d: d.geometry.apply(wkt.loads))
-                .pipe(gpd.GeoDataFrame, geometry='geometry', crs='EPSG:4326')
-                .assign(geometry=lambda d: d.geometry.to_crs('EPSG:4326'))
+        .assign(geometry=lambda d: d.geometry.apply(wkt.loads))
+        .pipe(gpd.GeoDataFrame, geometry='geometry', crs='EPSG:4326')
+        .assign(geometry=lambda d: d.geometry.to_crs('EPSG:4326'))
     )
 
 
 def test_add_to_trackmap(tmp_path, points_dataframe, lines_geodataframe, areas_geodataframe):
     m = TrackMap()
 
-    PlottingPoints(points_dataframe, popup=['name', 'name2']).add_to(m)
+    PlottingPoints(points_dataframe, popup=['name2']).add_to(m)
     PlottingLinestrings(lines_geodataframe, popup=['name2']).add_to(m)
     PlottingAreas(areas_geodataframe, popup=['name2']).add_to(m)
     total_objects = len(points_dataframe) + len(lines_geodataframe) + len(areas_geodataframe)
@@ -97,9 +101,9 @@ def test_add_to_trackmap(tmp_path, points_dataframe, lines_geodataframe, areas_g
     for m_child, q_child in zip(m._children, q._children):
         assert type(m_child) == type(q_child), 'Unequal type'
 
-    # Type is established with plottable, and popup is a list with 2 elements
+    # Popup is a list with 2 elements, and test some additional settings for linestrings and areas
     r = TrackMap()
-    PlottingPoints(points_dataframe, popup=['name', 'name2'], rotation_column='lat').add_to(r)
+    PlottingPoints(points_dataframe, popup=['name', 'name2']).add_to(r)
     PlottingLinestrings(lines_geodataframe, popup=['name', 'name2'], color='blue').add_to(r)
     PlottingAreas(areas_geodataframe, popup=['name', 'name2'], color='orange').add_to(r)
     r.save(tmp_path / 'test_output3.html')
@@ -108,10 +112,25 @@ def test_add_to_trackmap(tmp_path, points_dataframe, lines_geodataframe, areas_g
         assert type(q_child) == type(r_child), 'Unequal type'
 
 
+def test_plottingpoint_settings(points_dataframe, tmp_path):
+    m = TrackMap()
+    PlottingPoints(points_dataframe, popup=['name', 'name2'], rotation_column='lat').add_to(m)
+    PlottingPoints(points_dataframe, popup=['name', 'name2'], marker_column='marker').add_to(m)
+    PlottingPoints(points_dataframe, popup=['name', 'name2'], markertype='train').add_to(m)
+    PlottingPoints(points_dataframe, popup=['name', 'name2'], markertype='circle', radius_column='lat').add_to(m)
+    markers_added = len([child for child in m._children if child.split('_')[0] == 'marker'])
+    circles_added = len([child for child in m._children if child.split('_')[0] == 'circle'])
+
+    num_expected_markers = 3 * len(points_dataframe)
+    num_expected_circles = 1 * len(points_dataframe)
+    assert num_expected_markers == markers_added, 'Unexpected amount of objects'
+    assert num_expected_circles == circles_added, 'Unexpected amount of objects'
+
+
 def test_fix_zoom(prefilled_trackmap):
-    children_before = prefilled_trackmap.__dict__['_children'].copy()
+    children_before = prefilled_trackmap._children.copy()
     prefilled_trackmap._fix_zoom()
-    children_after = prefilled_trackmap.__dict__['_children'].copy()
+    children_after = prefilled_trackmap._children.copy()
     for child in children_before:
         try:
             if children_before[child]['_name'] == 'FitBounds':
@@ -122,7 +141,7 @@ def test_fix_zoom(prefilled_trackmap):
     fitbounds_found = False
     for child in children_after:
         try:
-            if children_after[child].__dict__['_name'] == 'FitBounds':
+            if children_after[child]._name == 'FitBounds':
                 fitbounds_found = True
         except TypeError:
             pass
